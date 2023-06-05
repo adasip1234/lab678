d78a333a677e5dcb8dc8f64ca549617ca26b763b
adasip1234 <131274760+adasip1234@users.noreply.github.com>
2023-06-05 22:41:18 +0200

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,1 +1,14 @@
+print("Konwerter plikow xml, .json i .yml (.yaml)")
+x = input("Podaj sciezke i rozszerzenie pliku: ")
+y = input("Podaj sciezke i rozszerzenie pliku do jakiego chcialbys zapisac plik: ")
+file_name, file_extension = os.path.splitext(x)
+file_name_new, file_extension_new = os.path.splitext(y)
 
+def sciezka():
+    sciezka = os.path.isfile(x)
+    if sciezka == True:
+        print("Zaraz nastopi konwetrowanie ")
+    else:
+        print("Bledna sciezka pliku")
+        quit()
+    return x, y
