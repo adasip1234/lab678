f1e3502fd552ded9086b0904d0b630c12a0c1db5
adasip1234 <131274760+adasip1234@users.noreply.github.com>
2023-06-05 22:43:05 +0200

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,1 +1,13 @@
+def xmlwjs(x, y):
+    data = task6(x)
+    donejson(y, data)
+    print("utworzono nowy plik .json")
 
+def ymlwjs(x, y):
+    data = task4(x)
+    donejson(y, data)
+    print("utworzono nowy plik .json")
+
+def donejson(x, data):
+    with open(x, "w") as f:
+        f.write(json.dumps(data))
