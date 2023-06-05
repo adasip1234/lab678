86eed9d1e93a7beb72744d364c7a671b5692631d
adasip1234 <131274760+adasip1234@users.noreply.github.com>
2023-06-05 22:43:39 +0200

    Update Task5.py

--- Task5.py
+++ Task5.py
@@ -1,1 +1,13 @@
+def jswyml(x, y):
+    data = task2(x)
+    doneyml(y, data)
+    print("utworzono nowy plik .yml")
 
+def xmlwyml(x, y):
+    data = task6(x)
+    doneyml(y, data)
+    print("utworzono nowy plik .yml")
+
+def doneyml(x, data):
+    with open(x, 'w') as f:
+        yaml.dump(data, f)
